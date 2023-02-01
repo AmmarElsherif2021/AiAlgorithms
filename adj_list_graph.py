d2515ed449b9425feed7c7a6da119403c15c4c23
AmmarElsherif2021 <ammar.alshareef2014@gmail.com>
2023-02-01 17:48:38 +0200

    modified

--- adj_list_graph.py
+++ adj_list_graph.py
@@ -4,16 +4,10 @@ Created on Wed Feb  1 15:20:59 2023
 
 @author: ammar
 """
-# Represent a graph>>
-
-#  1- Node class
-
-class Node:
-    def __init__ (self,data):
-        self.node=data
-        self.next=None
+from collections import defaultdict
+# Represent an undirected graph>>
     
-#  2-Graph class
+#  1-Graph class
 
 class Graph:
     
@@ -24,10 +18,48 @@ class Graph:
         self.N=nodes
         
         # Empty container for nodes >> a list for adjecency lists
-        self.graph=[None]*self.N
+        self.graph=defaultdict(list)
+        
+        # Fill the graph list
+        for node in self.N :
+            self.graph[node]
     
-    # function to make edges between nodes
-    def addEdge(self,source,dest):
+   
+    
+   # function to print graph
+    def print_graph(self):
+        for node in self.N:
+            print(node,' -->',self.graph[node])
+    
+   
+    
+   # function to make edges between nodes
+    def addEdge(self,s,d):
+        if d not in self.graph[s]:
+            self.graph[s].append(d)
+        else:
+            print(d , ' is connected to ',s)
         
+        if s not in self.graph[d]:
+            self.graph[d].append(s)
+        else:
+            print(s , ' is connected to ',d)
+    
+    #function to insert adjacencies:
+    def insertAdj(self):
+        for node in self.N:
+            for i in range(len(self.N)):
+                print('Now you inserts the node ',node, ' adjacency: >> >>')
+                adj=input('Enter adjacency')
+                if adj in self.N:
+                    graph.addEdge(node,adj)
+                else:
+                    break
+
         
+Nodes=['A','B','C','D','E']
+graph =Graph(Nodes)
+graph.insertAdj()
+graph.print_graph()
+
 
