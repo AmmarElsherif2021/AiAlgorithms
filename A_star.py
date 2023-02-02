3e1241ee4e03ea254c89950e067aeb2aa5db4c19
AmmarElsherif2021 <ammar.alshareef2014@gmail.com>
2023-02-02 14:53:13 +0200

    dir graph

---
+++ A_star.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 16:21:01 2023
+
+@author: ammar
+"""
+#A* search Algorithm
+#Greedy search Algorithm
+#Hill Climbing Algorithm
--- adj_list_graph.py
+++ adj_list_graph.py
@@ -5,7 +5,7 @@ Created on Wed Feb  1 15:20:59 2023
 @author: ammar
 """
 from collections import defaultdict
-# Represent an undirected graph>>
+# Represent an undirected graph  >>>>
     
 #  1-Graph class
 
@@ -50,16 +50,66 @@ class Graph:
         for node in self.N:
             for i in range(len(self.N)):
                 print('Now you inserts the node ',node, ' adjacency: >> >>')
-                adj=input('Enter adjacency')
+                adj=input('Enter adjacency  >')
                 if adj in self.N:
-                    graph.addEdge(node,adj)
+                    self.addEdge(node,adj)
                 else:
                     break
 
+  
+#Directional Graph
+
+class DiGraph:
+    
+    #constructor
+    def __init__ (self,nodes):
         
+        # num of nodes
+        self.N=nodes
+        
+        # Empty container for nodes >> a list for adjecency lists
+        self.graph=defaultdict(list)
+        
+        # Fill the graph list
+        for node in self.N :
+            self.graph[node]
+    
+   
+    
+   # function to print graph
+    def print_graph(self):
+        for node in self.N:
+            print(node,' -->',self.graph[node])
+    
+   
+    
+   # function to make edges between nodes
+    def addEdge(self,s,d):
+        if d not in self.graph[s]:
+            self.graph[s].append(d)
+        else:
+            print(d , ' is connected to ',s)
+        
+       
+    
+    #function to insert adjacencies:
+    def insertAdj(self):
+        for node in self.N:
+            for i in range(len(self.N)):
+                print('Now you inserts the node ',node, ' adjacency: >> >>')
+                adj=input('Enter adjacency  >')
+                if adj in self.N:
+                    self.addEdge(node,adj)
+                else:
+                    break
+#-implementation of undir-graph----------------------------------------------------------------      
 Nodes=['A','B','C','D','E']
-graph =Graph(Nodes)
-graph.insertAdj()
-graph.print_graph()
+#graph =Graph(Nodes)
+#graph.insertAdj()
+#graph.print_graph()
 
+#-implementation of dir-graph
 
+dirgraph =DiGraph(Nodes)
+dirgraph.insertAdj()
+dirgraph.print_graph()
