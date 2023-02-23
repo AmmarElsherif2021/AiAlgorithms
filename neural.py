bd952f9ca799fd81711eb4ecd89eeff68c0b40be
AmmarElsherif2021 <ammar.alshareef2014@gmail.com>
2023-02-23 17:40:15 +0200

    n.n

---
+++ neural.py
@@ -0,0 +1,8 @@
+import numpy as np
+'''
+Srcs:
+    https://realpython.com/python-ai-neural-network/
+    https://www.youtube.com/playlist?list=PLZHQObOWTQDNU6R1_67000Dx_ZCJB-3pi
+
+'''
+#use numpy to represent and do operations on vectors as arrays
